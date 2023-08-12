c49ee8648d8851aa3c891efae7cef83dff9a066f
Francesco De Lucchini <67149013+deluf@users.noreply.github.com>
2023-08-12 15:53:28 +0200

    Changed database configuration to MySQL defaults

--- php/config.php
+++ php/config.php
@@ -5,8 +5,8 @@ namespace Config;
 # Database configuration
 const dbHost = "127.0.0.1";
 const dbName = "codfish_2000";
-const dbUsername = "user";
-const dbPassword = "user";
+const dbUsername = "root";
+const dbPassword = "";
 const dbConnectionString = "mysql:host=".dbHost.";dbname=".dbName;
 
 # PHP session variables
